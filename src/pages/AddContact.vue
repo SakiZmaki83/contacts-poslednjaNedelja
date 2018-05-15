ed33b228684b08d8b39c35f0fad90ea343194cc3
jelena <jelena>
2018-05-15 19:20:36 +0200

    third

--- src/pages/AddContact.vue
+++ src/pages/AddContact.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="container mt-4">
-    <form 
-          v-validate="validationRules">
+    <form v-validate="validationConfig">
       <div class="form-group row">
         <label for="first_name" class="col-4 col-form-label">First Name</label>
         <div class="col-8">
           <div class="input-group">
-              <input  id="first_name" 
-                      name="first_name" 
-                      type="text"  
-                      class="form-control here" 
-                      v-model="contact.first_name">
+            <input
+              id="first_name"
+              name="first_name"
+              type="text"
+              class="form-control here"
+              v-model="contact.first_name"
+              v-focus-on>
           </div>
         </div>
       </div>
@@ -18,33 +19,34 @@
         <label for="last_name" class="col-4 col-form-label">Last Name</label>
         <div class="col-8">
           <div class="input-group">
-            <input  id="last_name" 
-                    name="last_name" 
-                    type="text" 
-                    class="form-control here" 
-                    v-model="contact.last_name">
+            <input id="last_name"
+                   name="last_name"
+                   type="text"
+                   class="form-control here"
+                   v-model="contact.last_name">
           </div>
         </div>
       </div>
       <div class="form-group row">
         <label for="email" class="col-4 col-form-label">Email</label>
         <div class="col-8">
-          <input  id="email" 
-                  name="email" 
-                  type="text"  
-                  class="form-control here" 
-                  v-model="contact.email">
+          <input
+            id="email"
+            name="email"
+            type="text"
+            class="form-control here"
+            v-model="contact.email">
         </div>
       </div>
       <div class="form-group row">
         <label for="number" class="col-4 col-form-label">Number</label>
         <div class="col-8">
           <div class="input-group">
-            <input  id="number" 
-                    name="number" 
-                    type="tel"  
-                    class="form-control here" 
-                    v-model="contact.number">
+            <input id="number"
+                   name="number"
+                   type="tel"
+                   class="form-control here"
+                   v-model="contact.number">
           </div>
         </div>
       </div>
@@ -54,13 +56,11 @@
         </div>
       </div>
     </form>
-    <div v-demo="'joj'"></div>
   </div>
 </template>
 
 <script>
 import { contacts } from '../services/Contacts'
-
 export default {
   data () {
     return {
@@ -70,13 +70,16 @@ export default {
         email: '',
         number: ''
       },
-      validationRules: {
-        email: ['required', 'email']
+      validationConfig: {
+        validationRules: {
+          email: [ 'required', 'email' ]
+        },
+        submitCallback: () => {
+          this.onSubmit()
+        }
+      }
     }
-    }
-    
   },
-
   created () {
     if (this.$route.params.id) {
       contacts.get(this.$route.params.id)
@@ -85,22 +88,19 @@ export default {
         })
     }
   },
-
   methods: {
     onSubmit () {
       this.$route.params.id ? this.editContact() : this.addContact()
     },
-
     editContact () {
       contacts.edit(this.contact)
-        .then((response) => {
+        .then(() => {
           this.$router.push('/contacts')
         })
     },
-
     addContact () {
       contacts.add(this.contact)
-        .then((response) => {
+        .then(() => {
           this.$router.push('/contacts')
         })
     }
--- src/plugins/MyDirectives.js
+++ src/plugins/MyDirectives.js
@@ -1,57 +1,87 @@
+import validator from 'validator'
+
+const RULES = {
+  REQUIRED: 'required',
+  NUMBER: 'number',
+  EMAIL: 'email'
+}
+const MESSAGES_CLASSNAME = 'validator-messages'
+
+const removeMessageErrorElements = (element) => {
+  let oldMessageElements =
+    element.querySelectorAll(`#${MESSAGES_CLASSNAME}`)
+
+  oldMessageElements.forEach((oldMessageElement) => {
+    oldMessageElement.remove()
+  })
+}
+
+const showMessageErrorElement = (element, message) => {
+  let messageElement = document.createElement('div')
+  messageElement.id = MESSAGES_CLASSNAME
+  messageElement.innerHTML = message
+  element.appendChild(messageElement)
+}
+
 const MyDirectives = {
-    install: function(Vue, options) {
-        Vue.directive('focus', {
-            inserted: function(element) {
-              element.focus()
+  install: function(Vue) {
+    Vue.directive('focusOn', {
+      inserted: function(element) {
+        element.focus()
+      }
+    })
+
+    Vue.directive('validate', {
+      inserted: function(element, binding) {
+        let validationConfig = binding.value
+        let validationRules = validationConfig.validationRules
+
+        element.addEventListener('submit', (event) => {
+          let errorCounter = 0
+          event.preventDefault()
+          Object.keys(validationRules).forEach(key => {
+            let input = element.querySelector(`#${key}`)
+            if (!input) {
+              throw new Error(
+                `Element for validation rule ${key} not found!`
+              )
             }
-          })
 
-          const RULES= {
-            REQUIRED: 'required',
-            NUMBER: 'number',
-            EMAIL: 'email'
-        }
-        const MESSAGES_CLASNAME = 'validator-messages'
-        const removeMessageErrorElement = (element) => {
-            let oldMessageElement = element.querySelector(`#${MESSAGES_CLASNAME}`)
+            // remove old message element
+            removeMessageErrorElements(element)
 
-                            if(oldMessageElement){
-                                oldMessageElement.remove()
-                            }
-        }
-        
-        Vue.directive('validate', {
-            inserted: function(element, binding){
-
-
-                let validationRules = binding.value
-                
-                element.addEventListener('submit', (event) => {
-                    event.preventDefault()
-                    Object.keys(validationRules).forEach(key => {
-                        let input = element.querySelector(`#${key}`)
-
-                        if(!input){
-                            throw new Error(`Element for validation rule ${key} not found.`)
-                        }
-
-                        if(validationRules[key].indexOf(RULES.REQUIRED) > -1 && !input.value.length){
-                            let messageElement = document.createElement('div')
-                            messageElement.id= MESSAGES_CLASNAME
-                            
-                            removeMessageErrorElement(element)
-
-                            messageElement.innerHTML = `${key} is required`
-                            element.appendChild(messageElement)
-                        }else{
-                            removeMessageErrorElement(element)
-                        }
-                    });
-                    
-                })
+            if (
+              validationRules[key].indexOf(RULES.EMAIL) > -1 &&
+              !validator.isEmail(input.value)
+            ) {
+              errorCounter++
+              showMessageErrorElement(
+                element,
+                `This field must be email`
+              )
             }
-        }) 
-    }
+
+            if (
+              validationRules[key].indexOf(RULES.REQUIRED) > -1 &&
+              !input.value.length
+            ) {
+              errorCounter++
+              showMessageErrorElement(
+                element,
+                `${key.toUpperCase()} field is required`
+              )
+            }
+
+
+          });
+
+          if (errorCounter === 0) {
+            validationConfig.submitCallback()
+          }
+        })
+      }
+    })
+  }
 }
 
 export default MyDirectives
